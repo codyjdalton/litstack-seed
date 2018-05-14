130d2bfeb0538760e3103d362fea5d113a6a3888
Cody Dalton <codydalton@Codys-MacBook-Pro.local>
2018-05-13 21:32:46 -0700

    travis: add port to index

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ import { LitCompiler } from '@litstack/core/dist/compiler';
 
 import { AppModule } from './modules/app.module';
 
-LitCompiler.bootstrap(AppModule);
+LitCompiler.bootstrap(AppModule, process.env.PORT);
